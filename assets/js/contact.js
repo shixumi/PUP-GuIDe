4fd6dd69e637331dcf628f4f1f5a6923e65e7ecb
shixumi <ldenzell88@gmail.com>
2024-02-10 03:10:01 +0800

    Re-added contact.js

---
+++ assets/js/contact.js
@@ -0,0 +1,12 @@
+<script>
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevents the form from submitting in the traditional way
+
+  // Add your logic here to handle form data (e.g., send it to a server using AJAX)
+
+  // Display a pop-up (alert) after submitting the form
+  alert("Form submitted successfully!");
+});
+
+</script>
+
